18626acc97e3fa44a366cc2df89e771f2728a2f0
mustafaalkhous <mustafa.alkhous@gmail.com>
2021-08-04 00:51:04 +0200

    Zerlegen von Strings

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -22,6 +22,26 @@ public class Main {
         System.out.println(s1.contains(s2)); // true
         System.out.println(s1.contains("ABC")); // false
 
+        //Zerlegen:
+
+        //public String substring(int startIndex)
+        //public String substring(int startIndex, int endIndex)
+        System.out.println(s1.substring(6));
+        System.out.println(s1.substring(6,7));
+
+        //public CharSequence subSequence(int startIndex, int endIndex)
+        //ist wie substring, aber sie hat zwei Param.
+
+        //public String[] split(String regex)
+
+        //public String[] split(String regex, int limit)
+        //Wo der Parameter limit ist, übergeben wir eine reguläre Zahl,
+        //die wir setzen, falls wir die Anzahl der Elemente des Arrays begrenzen möchten,
+        //die die Funktion zurückgibt.
+        String[] str = s1.split(" ");
+        System.out.println(str[0]);
+
+
 
 
     }
