ecf11b44d5c2103fdf76c571544ef96fa5857e48
mustafaalkhous <mustafa.alkhous@gmail.com>
2021-09-26 16:24:03 +0200

    Strings

--- src/Strings/Main.java
+++ src/Strings/Main.java
@@ -2,12 +2,10 @@ package Strings;
 
 public class Main {
     public static void main(String [] args){
-
-        // Finden :
-
         String s1 = "Hallo World" ;
         String s2 = "World" ;
 
+        // Finden :
         // charAt(..) :
         System.out.println(s1.charAt(1)); // -> a
         // indexOf(..) :
@@ -23,17 +21,13 @@ public class Main {
         System.out.println(s1.contains("ABC")); // false
 
         //Zerlegen:
-
         //public String substring(int startIndex)
         //public String substring(int startIndex, int endIndex)
         System.out.println(s1.substring(6));
         System.out.println(s1.substring(6,7));
-
         //public CharSequence subSequence(int startIndex, int endIndex)
         //ist wie substring, aber sie hat zwei Param.
-
         //public String[] split(String regex)
-
         //public String[] split(String regex, int limit)
         //Wo der Parameter limit ist, übergeben wir eine reguläre Zahl,
         //die wir setzen, falls wir die Anzahl der Elemente des Arrays begrenzen möchten,
@@ -42,19 +36,44 @@ public class Main {
         System.out.println(str[0]);
 
         //Ersetzen:
-
         //public String replace(String old, String new)
         System.out.println(s1.replace("World","Welt"));
-
         String s3 = "Musti KH 10 11 12";
-        System.out.println(s3.replaceFirst(" ","-"));
+        System.out.println(s3.replaceFirst(" ","-")); // => "Musti-KH 10 11 12"
 
+        //Vergleichen:
+        //public boolean startsWith(String prefix), ist präfix
+        //public boolean startsWith(String prefix, int pos), ist präfix ab Position ..
+        System.out.println(s1.startsWith("Hallo")); // => true
+        System.out.println(s1.startsWith("World",6)); // => true
+        //public boolean endsWith(String suffix), ist suffix
+        System.out.println(s1.endsWith("World")); // => true
+        //public boolean equals(Object anObject)
+        System.out.println(s1.equals(s2)); // => false
+        //public boolean equalsIgnoreCase(String str)
+        // => klein und groß Buchstaben werden ignoriert.
+        String s = "welcome";
+        String ss = new String("welcome");
+        System.out.println(s == ss); // (überprüft die Eigenschaft bzw. Objekt) => false
+        System.out.println(s.equals(ss)); // (überprüft die Zeichenkette) => true
 
-
-
-
-
-
-
+        //Verarbeiten:
+        //public String concat(String s) .. zusammenfügen (wie +)
+        String s4 = s1.concat(s2);
+        System.out.println(s4);
+        //public String valueOf(XYZ b)
+        char r1 []={'1', ' ', '2'};
+        String strr1 = "";
+        strr1 = String.valueOf(r1);
+        System.out.println(strr1);
+        //public char[] toCharArray() (Von String zu Array von Char's)
+        char r2 [];
+        String strr2= "Hallo";
+        r2 = strr2.toCharArray();
+        System.out.println(r2[1]);
+        //length(); Die Länge der Zeichenkette
+        System.out.println(s1.length());
+        //public String toLowerCase() => Die großbuchstaben werden kleinbuchstaben
+        //public String toUpperCase() => Die kleinbuchstaben werden großbuchstaben
     }
 }
