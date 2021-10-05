aa2486925f1f31494ad3ea042051f6b177b9de49
mustafaalkhous <mustafa.alkhous@gmail.com>
2021-10-05 09:08:29 +0200

    Random

--- src/Random/Main.java
+++ src/Random/Main.java
@@ -4,8 +4,18 @@ import java.util.Random;
 
 public class Main {
 
+    int erzeugeZufallZahl(int untereGrenze, int obereGrenze){
+        Random r = new Random();
+        int erg = r.nextInt(obereGrenze);
+        while (erg < untereGrenze){
+            erg = r.nextInt(obereGrenze);
+        }
+        return erg;
+    }
+
     public static void main(String[] args) {
 
+        Main m = new Main();
         Random random = new Random();
 
         // Generation einer zufälliger Zahl vom Typ int!
@@ -20,15 +30,13 @@ public class Main {
         boolean c = random.nextBoolean();
         System.out.println(c);
 
-        // Generation einer zufälliger Zahl vom Typ int zwischen 1 , 10.
-        // random.nextInt(10) : [0,10[ -> 10 wird nicht betrachtet.
-        // Wenn wir die 10 betrachten möchten, dann schreiben wir dies +1.
-
-        int d = random.nextInt(10) + 1 ; // ]1,10]
-        System.out.println(d);
-
-        int e = random.nextInt(11); // [0,10]
+        int e = random.nextInt(10); // [0,9] bzw. [0,10[
         System.out.println(e);
 
+        int d = random.nextInt(10) + 1 ; // [1,10] Die 0 ist nicht mehr zubetrachten.
+        System.out.println(d);
+
+        int e1 = m.erzeugeZufallZahl(20,30);
+        System.out.println(e1);
     }
 }
