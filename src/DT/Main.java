7c0db0a4d274b31549e9228aac3b6ed71f3dbf35
mustafaalkhous <mustafa.alkhous@gmail.com>
2021-10-29 12:09:53 +0200

    Java

--- src/DT/Main.java
+++ src/DT/Main.java
@@ -2,7 +2,29 @@ package DT;
 
 public class Main {
     public static void main(String[] args){
+
+        // primitive DT:
+        // Die Werte werden direkt aufs Stak angelegt.
+
+        byte a1 = 1; // 1 Byte
+        short a2 = 1; // 2 Byte
+        int a3 = 1; // 4 Byte
+        long a4 = 1; // 8 Byte
+        float b1 =  3f; //=> 3.0  (4Byte)
+        float b2 =  3.1f; //=> 3.1 (4Byte)
+        double d = 1.1; // 8 Byte
+        char c1 = 'a'; // 2 Byte
+        char c2 = 65; //=> A ; ASCII-Wrte
+        boolean e = true; // 2 Byte
+
+        // Referenz,Objekt DT:
+        // => Referenz auf Stak und verweisen auf die Objekte im Heap.
+
+        // Strings:
+        String s = "Hallo" ;
+        // Arrays:
+        int [] arr = {1,2,3};
+        // Objekte:
         String []a = new String[2];
-        System.out.println(a);
     }
 }
